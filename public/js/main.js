4f20028cd0222d34f87cc192a4373d161b30b681
Hyeji Jang <hyejij@uci.edu>
2020-06-04 22:04:14 -0400

    no message

--- public/js/main.js
+++ public/js/main.js
@@ -319,7 +319,25 @@ socket.on('game_update',function(payload){
     }
 
     $('#my_color').html('<h3 id="my_color">I am '+my_color+'</h3>');
-    $('#my_color').append('<h4>It is '+payload.game.whose_turn+'\'s turn </h4>');
+    $('#my_color').append('<h4>It is '+payload.game.whose_turn+'\'s turn. Elapsed time <span id="elapsed"></span> </h4>');
+
+    clearInterval(interval_timer);
+    interval_timer = setInterval(function(last_time){
+        return function(){
+            //Do the work of updating the UI
+            var d = new Date();
+            var elapsedmilli = d.getTime() - last_time;
+            var minutes = Math.floor(elapsedmilli / (60 * 1000));
+            var seconds = Math.floor((elapsedmilli % (60 * 1000))/ 1000);
+
+            if(seconds < 10){
+                $('#elapsed').html(minutes+':0'+seconds);
+            }
+            else{
+                $('#elapsed').html(minutes+':'+seconds);
+            }
+        }}(payload.game.last_move_time)
+        ,1000);
 
     /* Animate changes to the board */
 
@@ -367,10 +385,13 @@ socket.on('game_update',function(payload){
                 else{
                     $('#'+row+'_'+column).html('<img src="assets/images/error_new.gif" alt="error"/>');
                 }
+            }    
+            /* Set up interactivity */
+            $('#'+row+'_'+column).off('click');
+            $('#'+row+'_'+column).removeClass('hovered_over');
 
-                /* Set up interactivity */
-                $('#'+row+'_'+column).off('click');
-                if(board[row][column] == ' '){
+            if(payload.game.whose_turn === my_color){
+                if(payload.game.legal_moves[row][column] === my_color.substring(0,1)){
                     $('#'+row+'_'+column).addClass('hovered_over');
                     $('#'+row+'_'+column).click(function(r,c){
                         return function(){
@@ -382,9 +403,6 @@ socket.on('game_update',function(payload){
                             socket.emit('play_token',payload);
                         };
                     }(row,column));
-                }
-                else{
-                    $('#'+row+'_'+column).removeClass('hovered_over');
                 }
             }
         }   
